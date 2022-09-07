99fe9ee871defd06f004818a025e3aa5b321c651
ogodeverest <you@example.com>
2022-09-07 16:27:13 +0200

    Removed unused dir

--- 4fb833/src/index.js
+++
@@ -1,8 +0,0 @@
-import VillageState from "./village-state";
-import { randomRobot, routeRobot, goalOrientedRobot, runRobot } from "./robots";
-
-// runRobot(VillageState.random(5), randomRobot);
-
-// runRobot(VillageState.random(), routeRobot, []);
-
-runRobot(VillageState.random(), goalOrientedRobot, []);
--- 4fb833/src/utils.js
+++
@@ -1,4 +0,0 @@
-export function randomPick(array) {
-  const choice = Math.floor(Math.random() * array.length);
-  return array[choice];
-}
